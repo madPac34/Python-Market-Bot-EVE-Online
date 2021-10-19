496dad9568e31a9c9992160db7f3ce4ba0aaaaa1
Thomas Haacke <thomashaacke1@gmail.com>
2021-10-19 14:39:49 +0200

    Safety Check

--- Python Market Application/Python_Market_Application.py
+++ Python Market Application/Python_Market_Application.py
@@ -2,15 +2,42 @@ import pyautogui
 import time
 import random
 pyautogui.FAILSAFE = True #Slam mouse into top left to abort
-#XY coordinates have 0, 0 origin at top left corner of the screen.
+#XY coordinates have 0, 0 origin at top left corner of the screen. (1920, 1080)
+
+if pyautogui.locateCenterOnScreen('ticked.png') != None : #Unticks the Auto Update
+    move = pyautogui.locateCenterOnScreen('ticked.png')
+    pyautogui.moveTo(move)
+    pyautogui.click() 
+
+if pyautogui.locateCenterOnScreen('update.png') != None : #If an update is available, applies it 
+    move = pyautogui.locateCenterOnScreen('update.png')
+    pyautogui.moveTo(move)
+    pyautogui.click()
+    time.sleep(18)
+    while pyautogui.locateAllOnScreen('updatepromt') == None :
+        time.sleep(3)
+    else :
+        if pyautogui.locateAllOnScreen('updatepromt') != None :
+            time.sleep(1)
+            move = pyautogui.locateCenterOnScreen('ok.png')
+            pyautogui.moveTo(move)
+            pyautogui.click() 
+
+while pyautogui.locateCenterOnScreen('updating.png') != None : #If it is busy updating wait
+    time.sleep(5) 
+
+if pyautogui.locateCenterOnScreen('error.png') != None : #If there is an Error exit
+    exit() 
+
+
 
 
 for outbid in pyautogui.locateAllOnScreen('red1.png', confidence=0.9) :
     #JEveAssets
-    pyautogui.moveTo(outbid, duration=0.3)
-    pyautogui.moveRel(-50, 2, duration=0.1)
+    pyautogui.moveTo(outbid, )
+    pyautogui.moveRel(-50, 2)
     pyautogui.click()
-    time.sleep(random.uniform(1, 1.5))
+    time.sleep(random.uniform(1.2, 1.5))
 
     #Eve Client
     if pyautogui.locateCenterOnScreen('buy.png', confidence=0.99) == None :
@@ -63,4 +90,6 @@ for outbid in pyautogui.locateAllOnScreen('red1.png', confidence=0.9) :
                 pyautogui.moveTo(secondconfirmationrandom, duration=random.uniform(0.2, 0.5))
                 pyautogui.click()
                 time.sleep(random.uniform(0.8, 1.2))
+
+
 print('Done!')
